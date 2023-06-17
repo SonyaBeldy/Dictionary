b5677455d82974aa0c50c41d37fb3dec64c5f85a
Sonya <sofya.beldy@mail.ru>
2023-06-17 17:28:39 +1000

    first commit

---
+++ src/main/java/ru/sonyabeldy/dictionary/Dictionaty/DictionatyApplication.java
@@ -0,0 +1,13 @@
+package ru.sonyabeldy.dictionary.Dictionaty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionatyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionatyApplication.class, args);
+	}
+
+}
